9080a4ce77a1d1a124796bfbc718709c258f209d
splatoon7 <emichiou@pdx.edu>
2026-04-06 01:04:54 -0700

    attempt at cursor ani (need help!)

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 let hello = document.getElementById('TITLE');
 
 console.log(hello);
+
+const sparkles = () => {
+    let i = document.createElement('i');
+    i.style.left = (event.pageX) + 'px';
+    i.style.top = (event.pageY) + 'px';
+
+    i.style.scale = `${Math.random() * 2 + 1}`;
+
+    i.style.setProperty('--x', getTransitionValue()); 
+    i.style.setProperty('--y', getTransitionValue());
+
+    document.body.appendChild(i); 
+
+    setTimeout(() => {
+       document.body.removeChild(i);
+    }, 2000);
+
+}
+
+const getTransitionValue = () => {
+   return `${Math.random() * 400 - 200}px`;
+}
+document.addEventListener('mousemove', sparkles);
+
+//https://github.com/Avijit200318/Next-Level-Css/blob/main/cursor%20move%20animation/index.html
