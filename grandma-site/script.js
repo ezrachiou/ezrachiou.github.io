ab2df45cd974b2c571a6b8a21d102a86d4a69110
splatoon7 <emichiou@pdx.edu>
2026-05-04 12:38:11 -0700

    nav bar + base pages linked

--- grandma-site/script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('.hamburger')';
-const offScreenMenu = document.querySelector('.off-screen-menu');
-
-hamburger.addEventListener('click',() => {
-    hamburger.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
-})
